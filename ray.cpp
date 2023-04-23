bbfd38d511aec42ebd813e15990788e5ab9c8fd3
JimiUrsin <32343914+JimiUrsin@users.noreply.github.com>
2023-04-23 12:06:21 +0300

    Removed unnecessary attributes from implementations

--- ray.cpp
+++ ray.cpp
@@ -3,9 +3,9 @@
 ray::ray() = default;
 ray::ray(const point3d &origin, const vec3d& direction) : orig(origin), dir(direction) {}
 
-[[nodiscard]] point3d ray::origin() const { return orig; }
-[[nodiscard]] vec3d ray::direction() const { return dir; }
+point3d ray::origin() const { return orig; }
+vec3d ray::direction() const { return dir; }
 
-[[nodiscard]] point3d ray::at(float t) const {
+point3d ray::at(float t) const {
     return orig + dir * t;
 }
